cef604aebfe33f45dfdfaed90a9bcc2f69357c38
Jingling <mengwei@Jinglings-MacBook-Pro.local>
2026-07-05 15:46:03 -0400

    Fix missing colon in models

--- nn_approx_ladder/models.py
+++ nn_approx_ladder/models.py
@@ -17,7 +17,7 @@ import torch
 import torch.nn as nn
 
 
-def count_trainable(model: nn.Module) -> int
+def count_trainable(model: nn.Module) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
